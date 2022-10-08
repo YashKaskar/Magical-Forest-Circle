e076370d3f92a0a80fb1d481940d541a158e8fa5
Yash Kaskar <yash.kaskar99@gmail.com>
2022-10-08 15:35:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// I've provided you with some basic markup in index.html.  Please do not change any of the markup directly.
+// Instead, use JavaScript to make the following changes:
+
+// Select the div with the id of container.  Using JavaScript, set it's text alignment to 'center';
+
+// Select the image and use JavaScript to give it a width of 150px and a border radius of 50%
+
+
+const forest = document.querySelector('#container').style.textAlign = 'center';
+const forestImage = document.querySelector('img').style.width = '150px';
+const image = document.querySelector('img').style.borderRadius = '50%';
